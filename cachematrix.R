5306a04346b6265ea02ab2b706deb26416a47bd8
MJaken <poltinkin182@gmail.com>
2018-01-28 20:34:29 +0600

    Assignment done
    Solved assignment 2 of R programming course

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Create a "special" matrix - a list of functions and solve and store cached 
+## inverse of input matrix
 
-## Write a short comment describing this function
+
+## makeCacheMatrix FUN creates a list of functions inside an object and  
+## caches matrix itself and it's inverse if it was set or cached and solved by 
+## next function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+      inv <- NULL
+      
+      set <- function(y) {  ##FUN to set new matrix into "special" matrix
+            x <<- y
+            inv <<- NULL
+      }
+      
+      get <- function() x   ##Returns "special" matrix's inside matrix
+      
+      setinv <- function(inverse) inv <<- inverse ## Sets matrix's inverse cache
+      
+      getinv <- function() inv ##Returns cached inverse
+      
+      ##Finally creating list of FUNs that makes our "special" matrix
+      list(set = set, get = get, setinv = setinv, getinv = getinv) 
+      
 }
 
 
-## Write a short comment describing this function
+## cacheSolve FUN solves matrix's inverse and sets(caches) it to "special" matrix
+## in case it's not cached already
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+      inv <- x$getinv() ## Get cached inverse from "special" matrix
+      
+      if(!is.null(inv)) { ## Check if cached inverse is not NULL, if it' not:
+            message("getting cached data") ## Returning cached inverse 
+            return(inv)
+      }
+        else {
+            data <- x$get()  ## Get matrix 
+            inv <- solve(data, ...) ## Solve for it's inverse and load it to inv
+            x$setinv(inv) ## Set inverse to cache of "special" matrix
+            
+        }
+        inv ## Return a matrix that is the inverse of 'x'
 }
